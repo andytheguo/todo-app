586a40bb18f60f17f86c5d5e2602dba9d8f4c74a
andytheguo <263878363+andytheguo@users.noreply.github.com>
2026-08-04 22:09:40 +0100

    Setup Prisma with PostgreSQL

---
+++ server/prisma/migrations/20260804103044_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "complete" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,17 @@
 import express, { type Express, type Request, type Response } from 'express';
+import { prisma } from './lib/prisma.js';
 
 const app: Express = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
+});
+
+app.get("/test", async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+
+  res.json(users);
 });
 
 app.listen(port, () => {
