c92c3f4126bcc56f3ac88fccca88def6aa4a4fb0
andytheguo <263878363+andytheguo@users.noreply.github.com>
2026-08-05 08:21:00 +0100

    Added user creation endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,8 @@ import { prisma } from './lib/prisma.js';
 const app: Express = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
@@ -12,6 +14,19 @@ app.get("/test", async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
 
   res.json(users);
+});
+
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  const user = await prisma.user.create({
+    data: {
+      name: name,
+      email: email
+    },
+  })
+
+  res.status(201).json(user);
 });
 
 app.listen(port, () => {
