2166aaf57ba52b9cbb36bb57d175d1c867e83a22
andytheguo <263878363+andytheguo@users.noreply.github.com>
2026-08-07 22:15:12 +0100

    Added user login endpoint with jwt authentication

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { prisma } from '../lib/prisma.js';
 import { Prisma } from "../../generated/prisma/client.js";
 import argon2 from 'argon2';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
@@ -26,6 +27,45 @@ router.post("/users", async (req, res) => {
       return res.status(400).json({ error: "Missing or incorrect field" });
     }
 
+    res.sendStatus(500);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email) {
+      return res.status(400).json({ error: "email is required" });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email
+      }
+    });
+
+    if (!user || !password) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    const valid = await argon2.verify(user.password, password);
+
+    if (!valid) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    const secret = process.env.ACCESS_TOKEN_SECRET;
+
+    if (!secret) {
+      throw new Error("ACCESS_TOKEN_SECRET is undefined");
+    }
+
+    const accessToken = jwt.sign({ userId: user.id }, secret);
+
+    res.status(200).json({ accessToken: accessToken });
+  }
+  catch (e) {
     res.sendStatus(500);
   }
 });
