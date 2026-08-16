94c5f2a92e9482220f0bf044f7837e5e489d3da4
andytheguo <263878363+andytheguo@users.noreply.github.com>
2026-08-16 21:47:53 +0100

    Added task creation button and modal

--- client/src/main.ts
+++ client/src/main.ts
@@ -121,94 +121,139 @@ function displayLogin() {
 }
 
 function displayTasks(tasks: Task[]) {
-  if (app) {
-    app.innerHTML = `
+  if (!app) return;
+
+  app.innerHTML = `
+    <div id="tasks-header">
       <h1>Tasks</h1>
-      <div id="tasks">
-        <div id="incomplete-tasks">
-          <h2>In Progress</h2>
-        </div>
-        <div id="complete-tasks">
-          <h2>Completed</h2>
-        </div>
+      <button data-modal-target="#task-modal" id="add-button">Add Task</button>
+    </div>
+    <div id="tasks">
+      <div id="incomplete-tasks">
+        <h2>In Progress</h2>
       </div>
-      `;
+      <div id="complete-tasks">
+        <h2>Completed</h2>
+      </div>
+    </div>
+    <div class="modal" id="task-modal">
+      <div class="modal-header">
+        <h1>New Task</h1>
+        <button data-close-button>&times;</button>
+      </div>
+      <div class="modal-body">
+        <textarea id="task-title">Title</textarea>
+        <textarea id="task-description">Description...</textarea>
+        <button id="create-button">Create Task</button>
+      </div>
+    </div>
+    <div id="overlay"></div>
+    `;
 
-    const incompleteDiv = document.querySelector<HTMLDivElement>("#incomplete-tasks");
-    const completeDiv = document.querySelector<HTMLDivElement>("#complete-tasks");
+  const incompleteDiv = document.querySelector<HTMLDivElement>("#incomplete-tasks");
+  const completeDiv = document.querySelector<HTMLDivElement>("#complete-tasks");
 
-    const accessToken = localStorage.getItem("accessToken");
-    for (const task of tasks) {
-      const taskDiv = document.createElement("div");
-      taskDiv.classList = "task";
+  const accessToken = localStorage.getItem("accessToken");
+  for (const task of tasks) {
+    const taskDiv = document.createElement("div");
+    taskDiv.classList = "task";
 
-      const check = document.createElement("input");
-      check.type = "checkbox";
-      check.checked = task.complete;
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.checked = task.complete;
 
-      const delBtn = document.createElement("button");
-      delBtn.textContent = "DELETE";
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "DELETE";
 
-      check.addEventListener("change", async () => {
-        try {
-          const res = await fetch(`http://localhost:3000/user/tasks/${task.id}`, {
-            method: "PATCH",
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": `Bearer ${accessToken}`
-            },
-            body: JSON.stringify({
-              complete: check.checked
-            })
-          });
+    check.addEventListener("change", async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/user/tasks/${task.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${accessToken}`
+          },
+          body: JSON.stringify({
+            complete: check.checked
+          })
+        });
 
-          if (!res.ok) {
-            const data = await res.json();
-            throw new Error(data.error);
-          }
-
-          changeState("tasks");
+        if (!res.ok) {
+          const data = await res.json();
+          throw new Error(data.error);
         }
-        catch (e) {
-          console.error(e);
-        }
-      });
 
-      delBtn.addEventListener("mouseup", async () => {
-        try {
-          const res = await fetch(`http://localhost:3000/user/tasks/${task.id}`, {
-            method: "DELETE",
-            headers: {
-              "Authorization": `Bearer ${accessToken}`
-            }
-          });
-
-          if (!res.ok) {
-            const data = await res.json();
-            throw new Error(data.error);
-          }
-
-          changeState("tasks");
-        }
-        catch (e) {
-          console.error(e);
-        }
-      });
-
-      const title = document.createElement("h2");
-      title.textContent = task.title;
-
-      let description;
-      if (task.description) {
-        description = document.createElement("p");
-        description.textContent = task.description;
+        changeState("tasks");
       }
+      catch (e) {
+        console.error(e);
+      }
+    });
 
-      taskDiv.append(check, title, description, delBtn);
+    delBtn.addEventListener("mouseup", async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/user/tasks/${task.id}`, {
+          method: "DELETE",
+          headers: {
+            "Authorization": `Bearer ${accessToken}`
+          }
+        });
 
-      (task.complete ? completeDiv : incompleteDiv).appendChild(taskDiv);
+        if (!res.ok) {
+          const data = await res.json();
+          throw new Error(data.error);
+        }
+
+        changeState("tasks");
+      }
+      catch (e) {
+        console.error(e);
+      }
+    });
+
+    const title = document.createElement("h2");
+    title.textContent = task.title;
+
+    let description;
+    if (task.description) {
+      description = document.createElement("p");
+      description.textContent = task.description;
     }
+
+    taskDiv.append(check, title, description, delBtn);
+
+    (task.complete ? completeDiv : incompleteDiv).appendChild(taskDiv);
   }
+
+  const openModalButtons = document.querySelectorAll<HTMLButtonElement>("[data-modal-target]");
+  const closeModalButtons = document.querySelectorAll<HTMLButtonElement>("[data-close-button]");
+  const overlay = document.querySelector<HTMLDivElement>("#overlay");
+
+  openModalButtons.forEach(button => {
+    button.addEventListener("mouseup", () => {
+      const modal = document.querySelector(button.dataset.modalTarget);
+      openModal(modal, overlay);
+    });
+  });
+
+  closeModalButtons.forEach(button => {
+    button.addEventListener("mouseup", () => {
+      const modal = button.closest(".modal");
+      closeModal(modal, overlay);
+    });
+  });
+}
+
+function openModal(modal, overlay) {
+  if (!modal) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal(modal, overlay) {
+  if (!modal) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
 }
 
 // TODO: Need to handle errors from getTasks()
